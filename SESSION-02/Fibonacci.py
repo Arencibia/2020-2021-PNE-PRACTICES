2ce80805237de09e4c7555f4b67999efbc0fc257
daren <D.arencibia.2020>
2021-02-17 17:34:52 +0100

    Fibonacci first 11 numbers

--- SESSION-02/Fibonacci.py
+++ SESSION-02/Fibonacci.py
@@ -1,9 +1,9 @@
 a=0
 b=1
-fibonaccilist=[a, b]
+fibonacciNums=[a, b]
 for i in range (1, 11):
     c=a+b
-    fibonaccilist.append(c)
+    fibonacciNums.append(c)
     a=b
     b=c
-print('The fibonacci numbers are', fibonaccilist)
+print('The first 11 fibonacci numbers are', fibonacciNums)
