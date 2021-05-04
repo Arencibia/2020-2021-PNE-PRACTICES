d1a197ac49b6f7d5706ac3e8fe7e863761c9a58a
daren <D.arencibia.2020>
2021-05-04 03:20:38 +0200

    Practice 6

--- P6/server2.py
+++ P6/server2.py
@@ -1,90 +1,81 @@
 import http.server
 import socketserver
 import termcolor
-import pathlib
-import jinja2
+from urllib.parse import urlparse, parse_qs
+import server_utils as su
 
-def read_html_file(filename):
-    contents = pathlib.Path(filename).read_text()
-    return contents
+PORT = 8080
 
-def read_template_html_file(filename):
-    contents = jinja2.Template(pathlib.Path(filename).read_text())
-    return contents
-# Define the Server's port
-PORT = 8087
+list_sequences = ['ACCGTGGTGTAACGAAA', 'ATTTGCTGTCTCT', 'CTCTCTCGAGAGAG', 'TACTCGGCCG', 'CGCGTAGGGATGACGTAGC']
+list_genes = ['ADA', 'FRAT1', 'FXN', 'RNU6_269P', 'U5']
 
 BASES_INFORMATION = {
-    "A":{"link": "https://es.wikipedia.org/wiki/Adenina" ,
-        "formula": "C5H5N5",
-        "name": "ADENINE",
-        "color": "green"
+    "A": {"link": "https://en.wikipedia.org/wiki/Adenine",
+          "formula": "C5H5N5",
+          "name": "ADENINE",
+          "color": "lightgreen"
     },
-    "C": {"link": "https://es.wikipedia.org/wiki/Citosina",
-          "formula": "C4H5N30",
+    "C": {"link": "https://en.wikipedia.org/wiki/Cytosine",
+          "formula": "C4H5N3O",
           "name": "CYTOSINE",
           "color": "yellow"
-    },
-    "G" : {"link":"https://es.wikipedia.org/wiki/Guanina",
-           "formula":"C5N5H5",
-           "name": "GUANINE",
-           "color":"lightskyblue"
-    },
-    "T": {"link": "https://es.wikipedia.org/wiki/Timina",
-          "formula":"C5H6N202",
+          },
+    "G": {"link": "https://en.wikipedia.org/wiki/Guanine",
+          "formula": "C5H5N5O",
+          "name": "GUANINE",
+          "color": "lightskyblue"
+          },
+    "T": {"link": "https://en.wikipedia.org/wiki/Thymine",
+          "formula": "C5H6N2O2",
           "name": "THYMINE",
-          "color":"lightpink"
-    }
-
+          "color": "lightpink"
+          }
 }
 
-
-# -- This is for preventing the error: "Port already in use"
-socketserver.TCPServer.allow_reuse_address = True
-
-
-# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
-# It means that our class inheritates all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
-
     def do_GET(self):
-        """This method is called whenever the client invokes the GET method
-        in the HTTP protocol request"""
 
-        # Print the request line
         termcolor.cprint(self.requestline, 'green')
         termcolor.cprint(self.path, 'blue')
 
+        o = urlparse(self.path)
+        path_name = o.path
+        arguments = parse_qs(o.query)
+        print("Resource requested: ", path_name)
+        print("Parameters:", arguments)
 
-        # IN this simple server version:
-        # We are NOT processing the client's request
-        # It is a happy server: It always returns a message saying
-        # that everything is ok
+        context = {}
 
-        if self.path == "/":
-            contents = read_html_file("./html/index.html")
-        elif "/info/" in self.path:
-            base = self.path.split("/")[-1]
-            context = BASES_INFORMATION[base]
-            context["letter"] = base
-            contents = read_template_html_file("./html/info/general.html").render(base_information= context)
-
-        elif self.path.endswith(".html"):
-            try:
-                contents = read_html_file("./html" + self.path)
-            except FileNotFoundError:
-                contents = read_html_file("./html/error.html")
+        if path_name == "/":
+            context['n_sequences'] = len(list_sequences)
+            context['list_genes'] = list_genes
+            contents = su.read_template_html_file('./html/index.html').render(context=context)
+        elif path_name == '/test':
+            contents = su.read_template_html_file('./html/test.html').render()
+        elif path_name == '/ping':
+            contents = su.read_template_html_file('./html/ping.html').render()
+        elif path_name == '/get':
+            number_sequence = arguments['sequence'][0]
+            contents = su.get(list_sequences, number_sequence)
+        elif path_name == '/gene':
+            gene = arguments['gene'][0]
+            contents = su.gene(gene)
+        elif path_name == '/operation':
+            sequence = arguments['sequence'][0]
+            operation = arguments['calculation'][0]
+            if operation == 'Info':
+                contents = su.info(sequence)
+            elif operation == 'Comp':
+                contents = su.comp(sequence)
+            elif operation == 'Rev':
+                contents = su.rev(sequence)
         else:
-            contents = read_html_file("./html/error.html")
-        # Message to send back to the clinet
-
-
-        # Generating the response message
-        self.send_response(200)  # -- Status line: OK!
+            contents = su.read_template_html_file('./html/error.html').render()
+        self.send_response(200)
 
         # Define the content-type header:
-        self.send_header('Content-Type', 'text/html') # Si no ponemos html saldrá texto!!
+        self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(contents.encode()))
 
         # The header is finished
@@ -94,24 +85,12 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write(contents.encode())
 
         return
-
-
-# ------------------------
-# - Server MAIN program
-# ------------------------
-# -- Set the new handler
 Handler = TestHandler
-
-# -- Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-
     print("Serving at PORT", PORT)
-
-    # -- Main loop: Attend the client. Whenever there is a new
-    # -- clint, the handler is called
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         print("")
-        print("Stoped by the user")
+        print("Stopped by the user")
         httpd.server_close()
